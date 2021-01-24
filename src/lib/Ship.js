c67e65cefcf72ab8dcba357b5868c07eaec66147
Samuel McKean <samuel.mckean@gmail.com>
2021-01-24 11:30:28 -0600

    added Ship factory function

--- src/lib/Ship.js
+++ src/lib/Ship.js
@@ -1,5 +1,33 @@
-const Ship = () => {
+const Ship = (length) => {
+  // initialize hitStatus array
+  const hitStatus = new Array(length);
+  for (let i = 0; i < length; i++) {
+    hitStatus[i] = 0;
+  }
 
+  const hit = (hitIndex) => {
+    // check if spot has been hit before
+    if (hitStatus[hitIndex] === 1) {
+      throw new Error('Invalid move');
+    }
+    hitStatus[hitIndex] = 1
+  };
+
+  const isSunk = () => {
+    for (let i = 0; i < length; i++) {
+      if (hitStatus[i] === 0) {
+        return false;
+      }
+      return true;
+    }
+  };
+
+  return {
+    length,
+    hitStatus,
+    hit,
+    isSunk,
+  };
 }
 
 export default Ship;
--- src/lib/Ship.test.js
+++ src/lib/Ship.test.js
@@ -1,5 +1,42 @@
 import Ship from './Ship';
 
 test('Ship returns an object', () => {
-  expect(typeof Ship()).toBe('object');
+  expect(typeof Ship(1)).toBe('object');
+});
+
+test('Ship length matches argument', () => {
+  const ship = Ship(1);
+  expect(ship.length).toBe(1);
+});
+
+test('Last element on hitStatus is zero to start', () => {
+  const ship = Ship(3);
+  expect(ship.hitStatus[2]).toBe(0);
+});
+
+test('Hit function updates only that spot in hitStatus with a 1', () => {
+  const ship = Ship(3);
+  ship.hit(1);
+  expect(ship.hitStatus[1]).toBe(1);
+  expect(ship.hitStatus[0]).toBe(0);
+  expect(ship.hitStatus[2]).toBe(0);
+});
+
+test('Calling hit with an index that has already been hit returns an error', () => {
+  const ship = Ship(3);
+  ship.hit(1);
+  expect(() => ship.hit(1)).toThrowError();
+});
+
+test('isSunk returns false when only one spot of 3 has been hit', () => {
+  const ship = Ship(3);
+  ship.hit(1);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test('isSunk returns true when all spots have been hit', () => {
+  const ship = Ship(2);
+  ship.hit(0);
+  ship.hit(1);
+  expect(ship.isSunk()).toBe(true);
 });
